257729a3efb86a8b4a26c24accdc732ee761953d
Olagoke Okebukola <gokuzimaki@gmail.com>
2024-08-30 12:23:40 +0100

    Update Frame.php

--- src/PhpWord/Writer/Word2007/Style/Frame.php
+++ src/PhpWord/Writer/Word2007/Style/Frame.php
@@ -42,7 +42,7 @@ class Frame extends AbstractStyle
         $xmlWriter = $this->getXmlWriter();
 
         $maxZIndex = min(PHP_INT_MAX, self::PHP_32BIT_INT_MAX);
-        $zIndices = [FrameStyle::WRAP_INFRONT => $maxZIndex, FrameStyle::WRAP_BEHIND => -$maxZIndex];
+        $zIndices = [FrameStyle::WRAP_INFRONT => $maxZIndex, FrameStyle::WRAP_BEHIND => 0];
 
         $properties = [
             'width' => 'width',
